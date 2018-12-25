939e33118e415cd6943caa2485b8c1607001444d
ratnadeepbiswakarma <ratnadeeppersonal@gmail.com>
2018-12-25 21:25:06 +0530

    Book component devided

---
+++ src/components/Book/Book.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+
+class Book extends Component {
+  render() {
+    const book = this.props.book;
+    return (
+      <li key={book.id}>
+        <div className="book">
+          <div className="book-top">
+            <div
+              className="book-cover"
+              style={{
+                width: 128,
+                height: 193,
+                backgroundImage: `url(${book.imageLinks.smallThumbnail})`
+              }}
+            />
+            <div className="book-shelf-changer">
+              <select
+                defaultValue={book.shelf}
+                onChange={event => this.props.handleSelection(event, book)}
+              >
+                <option value="move" disabled>
+                  Move to...
+                </option>
+                <option value="currentlyReading">
+                  {this.shelf === "currentlyReading"
+                    ? "Currently Reading 🗸"
+                    : "Currently Reading"}
+                </option>
+                <option value="wantToRead">
+                  {this.shelf === "wantToRead"
+                    ? "Want to Read 🗸"
+                    : "Want to Read"}
+                </option>
+                <option value="read">
+                  {this.shelf === "read" ? "Read 🗸" : "Read"}
+                </option>
+                <option value="none">Remove Book</option>
+              </select>
+            </div>
+          </div>
+          <div className="book-title">{book.title}</div>
+          <div className="book-authors">
+            <strong>V: </strong>
+            {book.contentVersion}
+          </div>
+          <div className="book-authors">{book.authors}</div>
+          <div className="book-authors">Pages: {book.pageCount}</div>
+          <div className="book-authors">
+            Published by: {book.publisher} on {book.publishedDate}
+          </div>
+        </div>
+      </li>
+    );
+  }
+}
+
+export default Book;
--- src/components/shelf/Shelf.jsx
+++ src/components/shelf/Shelf.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Book from "./../Book/Book";
 
 class Shelf extends Component {
   books = this.props.books;
@@ -16,60 +17,10 @@ class Shelf extends Component {
                 .filter(item => item.shelf === this.shelf)
                 .map(book => {
                   return (
-                    <li key={book.id}>
-                      <div className="book">
-                        <div className="book-top">
-                          <div
-                            className="book-cover"
-                            style={{
-                              width: 128,
-                              height: 193,
-                              backgroundImage: `url(${
-                                book.imageLinks.smallThumbnail
-                              })`
-                            }}
-                          />
-                          <div className="book-shelf-changer">
-                            <select
-                              defaultValue={book.shelf}
-                              onChange={event =>
-                                this.props.handleSelection(event, book)
-                              }
-                            >
-                              <option value="move" disabled>
-                                Move to...
-                              </option>
-                              <option value="currentlyReading">
-                                {this.shelf === "currentlyReading"
-                                  ? "Currently Reading 🗸"
-                                  : "Currently Reading"}
-                              </option>
-                              <option value="wantToRead">
-                                {this.shelf === "wantToRead"
-                                  ? "Want to Read 🗸"
-                                  : "Want to Read"}
-                              </option>
-                              <option value="read">
-                                {this.shelf === "read" ? "Read 🗸" : "Read"}
-                              </option>
-                              <option value="none">Remove Book</option>
-                            </select>
-                          </div>
-                        </div>
-                        <div className="book-title">{book.title}</div>
-                        <div className="book-authors">
-                          <strong>V: </strong>
-                          {book.contentVersion}
-                        </div>
-                        <div className="book-authors">{book.authors}</div>
-                        <div className="book-authors">
-                          Pages: {book.pageCount}
-                        </div>
-                        <div className="book-authors">
-                          Published by: {book.publisher} on {book.publishedDate}
-                        </div>
-                      </div>
-                    </li>
+                    <Book
+                      book={book}
+                      handleSelection={this.props.handleSelection}
+                    />
                   );
                 })}
             </ol>
